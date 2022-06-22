189aaafb986766c9ae33c186fa156d274e997a6d
Vadim <fates@duck.com>
2022-06-22 10:14:22 +0300

    feat: add package generation for cross-compile script

--- cross-compile.py
+++ cross-compile.py
@@ -8,6 +8,7 @@ VERSION = "1.5.1"
 TAG = f"v{VERSION}"
 DESCRIPTION = "The ultimate CLI manga downloader"
 GITHUB = "https://github.com/metafates/mangal"
+LICENSE = "MIT"
 BIN = "mangal"
 GOMOD = "github.com/metafates/mangal"
 PLATFORMS = {
@@ -48,11 +49,12 @@ def compile_for(goos):
         subprocess.call([
             "go", "build",
             "-o", target,
+            "-ldflags=-s -w",
             GOMOD
         ])
 
 
-def make_scoop_manifest():
+def generate_scoop_manifest():
     win32bin = f"{BIN}-windows-386.exe"
     win64bin = f"{BIN}-windows-amd64.exe"
 
@@ -60,7 +62,7 @@ def make_scoop_manifest():
         "version": VERSION,
         "description": DESCRIPTION,
         "homepage": GITHUB,
-        "license": "MIT",
+        "license": LICENSE,
         "architecture": {
             "32bit": {
                 "hash": sha256(os.path.join("bin", win32bin)),
@@ -78,16 +80,85 @@ def make_scoop_manifest():
                      },
         "autoupdate": {
             "32bit": {
-                "url": f"{GITHUB}/releases/download/v$version/{BIN}-windows-386.exe"
+                "url": f"{GITHUB}/releases/download/{TAG}/{win32bin}"
             },
             "64bit": {
-                "url": f"{GITHUB}/releases/download/v$version/{BIN}-windows-amd64.exe"
+                "url": f"{GITHUB}/releases/download/{TAG}/{win64bin}"
             }
         }
     }
 
-    with open(os.path.join("bin", "mangal.json"), 'w') as scoopFile:
+    with open(os.path.join("bin", f"{BIN}.json"), 'w') as scoopFile:
         json.dump(manifest, scoopFile, indent=4)
+
+
+def generate_homebrew_formula():
+    formula = f"""
+class {BIN.title()} < Formula
+    desc "{DESCRIPTION}"
+    homepage "{GITHUB}"
+    url "{GITHUB}", :using => :git, :tag => "{TAG}"
+    version "{VERSION}"
+    sha256 "92da0f4a880f86a5d782f47cf912f0206e2d49c5fcc27d57931ce0ef96e85029"
+    license "{LICENSE}"
+
+    depends_on "go" => :build
+
+    def install
+        system "go", "build", *std_go_args(ldflags: "-s -w")
+    end
+
+    test do
+        system "true"
+    end
+end
+    """
+
+    with open(os.path.join("bin", f"{BIN}.rb"), 'w') as f:
+        f.write(formula)
+
+
+# Was not tested
+def generate_pkgbuild():
+    go_version = "go>=1.8.0"
+    pkbuild = f"""
+# Maintainer: metafates <fates@duck.com>
+pkgname={BIN}
+pkgver={VERSION}
+pkgrel=1
+pkgdesc="{DESCRIPTION}"
+arch=('i686' 'x86_64')
+url="{GITHUB}"
+license=('{LICENSE}')
+depends=('glibc')
+makedepends=('{go_version}')
+checkdepends=('{go_version}')
+source=("{GITHUB}/archive/refs/tags/v{VERSION}.tar.gz")
+
+prepare() {{
+    cd "{BIN}-{VERSION}"
+    mkdir -p build/
+}}
+
+build() {{
+    cd "{BIN}-{VERSION}"
+    export CGO_CPPFLAGS="${{CPPFLAGS}}"
+    export CGO_CFLAGS="${{CFLAGS}}"
+    export CGO_CXXFLAGS="${{CXXFLAGS}}"
+    export CGO_LDFLAGS="${{LDFLAGS}}"
+    export GOFLAGS="-buildmode=pie -trimpath -ldflags=-linkmode=external -mod=readonly -modcacherw"
+    go get -d ./...
+    go build -o build ./...
+}}
+
+package() {{
+    cd "{BIN}-{VERSION}"
+    install -Dm755 build/{BIN} "{BIN}"/usr/bin/{BIN}
+}}
+"""
+
+    with open(os.path.join("bin", "PKGBUILD"), 'w') as f:
+        f.write(pkbuild)
 
 
 def main():
@@ -96,8 +167,14 @@ def main():
         print(f"Compiling for {goos.title()}")
         compile_for(goos)
 
-    print("Generating scoop manifest")
-    make_scoop_manifest()
+    print("Generating Scoop Manifest")
+    generate_scoop_manifest()
+
+    print("Generating Homebrew Formula")
+    generate_homebrew_formula()
+
+    print("Generating PKGBUILD")
+    generate_pkgbuild()
 
     print("Done")
 
