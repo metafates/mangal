92a00457f1cb07e925e6a0a6f72bf609110f738f
Vadim <fates@duck.com>
2022-06-22 14:26:50 +0300

    docs: add more comments

--- cross-compile.py
+++ cross-compile.py
@@ -9,8 +9,17 @@ TAG = f"v{VERSION}"
 DESCRIPTION = "The ultimate CLI manga downloader"
 GITHUB = "https://github.com/metafates/mangal"
 LICENSE = "MIT"
+
+# Required go version
+GO_VERSION = ">=1.8.0"
+
+# Binary name
 BIN = "mangal"
+
+# Url of go module
 GOMOD = "github.com/metafates/mangal"
+
+# Platforms to compile for
 PLATFORMS = {
     "windows": {
         "arch": ["arm", "arm64", "amd64", "386"],
@@ -28,6 +37,11 @@ PLATFORMS = {
 
 
 def sha256(filename):
+    """
+    Generate SHA256 hash for file
+    :param filename: file name
+    :return: SHA256 hash
+    """
     chunks = []
     with open(filename, "rb") as f:
         # Read and update hash string value in blocks of 4K
@@ -38,6 +52,10 @@ def sha256(filename):
 
 
 def compile_for(goos):
+    """
+    Compile for a given platform
+    :param goos: platform to compile for
+    """
     os.environ["GOOS"] = goos
 
     ext = PLATFORMS[goos]["ext"]
@@ -55,6 +73,9 @@ def compile_for(goos):
 
 
 def generate_scoop_manifest():
+    """
+    Generate Scoop manifest for Scoop
+    """
     win32bin = f"{BIN}-windows-386.exe"
     win64bin = f"{BIN}-windows-amd64.exe"
 
@@ -93,6 +114,9 @@ def generate_scoop_manifest():
 
 
 def generate_homebrew_formula():
+    """
+    Generate Homebrew formula for Homebrew
+    """
     formula = f"""
 class {BIN.title()} < Formula
     desc "{DESCRIPTION}"
@@ -118,9 +142,10 @@ end
         f.write(formula)
 
 
-# Was not tested
 def generate_pkgbuild():
-    go_version = "go>=1.8.0"
+    """
+    Generate PKGBUILD file for pkgbuild.org
+    """
     pkbuild = f"""
 # Maintainer: metafates <fates@duck.com>
 pkgname={BIN}
@@ -131,8 +156,8 @@ arch=('i686' 'x86_64')
 url="{GITHUB}"
 license=('{LICENSE}')
 depends=('glibc')
-makedepends=('{go_version}')
-checkdepends=('{go_version}')
+makedepends=('go {GO_VERSION}')
+checkdepends=('go {GO_VERSION}')
 source=("{GITHUB}/archive/refs/tags/v{VERSION}.tar.gz")
 
 prepare() {{
@@ -161,8 +186,102 @@ package() {{
         f.write(pkbuild)
 
 
+def generate_deb_package_for(architecture):
+    """
+    Generates a deb package for the given architecture.
+    :param architecture: The architecture to generate the deb package for.
+    """
+
+    if architecture == "386":
+        metadata_architecture = "i386"
+    elif architecture == "amd64":
+        metadata_architecture = "amd64"
+    elif architecture == "arm64":
+        metadata_architecture = "arm64"
+    elif architecture == "arm":
+        metadata_architecture = "armhf"
+    else:
+        raise Exception(f"Unknown architecture: {architecture}")
+
+    metadata = f"""
+Package: {BIN}
+Version: {VERSION}
+License: {LICENSE}
+Section: utils
+Priority: optional
+Architecture: {metadata_architecture}
+Description: {DESCRIPTION}
+Maintainer: metafates <fates@duck.com>
+"""
+
+    major_version = VERSION.split(".")[0]
+    minor_version = VERSION.split(".")[1]
+    revision = VERSION.split(".")[2]
+
+    package_name = f"{BIN}_{major_version}.{minor_version}-{revision}_{metadata_architecture}"
+
+    # make directory with package name at bin folder
+    os.mkdir(os.path.join("bin", package_name))
+
+    # make usr/local/bin folder at bin/package_name folder
+    os.makedirs(os.path.join("bin", package_name, "usr", "local", "bin"))
+
+    # copy binary to usr/local/bin folder
+    shutil.copy(os.path.join("bin", f"{BIN}-linux-{architecture}"),
+                os.path.join("bin", package_name, "usr", "local", "bin", BIN))
+
+    # make DEBIAN folder at bin/package_name folder
+    os.mkdir(os.path.join("bin", package_name, "DEBIAN"))
+
+    # move metadata to bin/package_name/DEBIAN/control
+    with open(os.path.join("bin", package_name, "DEBIAN", "control"), 'w') as f:
+        f.write(metadata)
+
+    # build deb package with architecture in name
+    subprocess.call(["dpkg-deb", "-b", os.path.join("bin", package_name)])
+
+    # remove bin/package_name folder
+    shutil.rmtree(os.path.join("bin", package_name))
+
+
+def generate_deb_packages():
+    # check if dpkg-deb is installed
+    try:
+        subprocess.check_output(["dpkg-deb", "--version"])
+    except subprocess.CalledProcessError:
+        print("dpkg-deb is not installed. Please install dpkg-deb to generate a deb package.")
+        return
+
+    for arch in PLATFORMS["linux"]["arch"]:
+        generate_deb_package_for(arch)
+
+
+def generate_checksums():
+    """
+    Generate checksums for all files in bin folder
+    """
+    with open(os.path.join("bin", "sha256sums.txt"), 'w') as f:
+        for file in os.listdir(os.path.join("bin")):
+            skip_conditions = [
+                file.endswith(".json"),
+                file == "sha256sums.txt",
+                file == "PKGBUILD",
+                file.endswith(".rb")
+            ]
+
+            if any(skip_conditions):
+                continue
+
+            checksum = sha256(os.path.join("bin", file))
+            f.write(f"{checksum} {file}\n")
+
+
 def main():
-    shutil.rmtree("bin")
+    # delete bin folder if it exists
+    if os.path.exists(os.path.join("bin")):
+        shutil.rmtree(os.path.join("bin"))
+
+    # Cross compile for all platforms listed in PLATFORMS
     for goos in PLATFORMS:
         print(f"Compiling for {goos.title()}")
         compile_for(goos)
@@ -175,6 +294,12 @@ def main():
 
     print("Generating PKGBUILD")
     generate_pkgbuild()
+
+    print("Generating Debian Packages")
+    generate_deb_packages()
+
+    print("Generating Checksums")
+    generate_checksums()
 
     print("Done")
 
