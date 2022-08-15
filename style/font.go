49b2d39324bcd345fa92eed3d308d344c3a50a8a
metafates <fates@duck.com>
2022-08-15 18:23:11 +0400

    build: update deps

--- style/font.go
+++ style/font.go
@@ -6,4 +6,5 @@ var (
 	Bold      = lipgloss.NewStyle().Bold(true).Render
 	Italic    = lipgloss.NewStyle().Italic(true).Render
 	Underline = lipgloss.NewStyle().Underline(true).Render
+	Faint     = lipgloss.NewStyle().Faint(true).Render
 )
