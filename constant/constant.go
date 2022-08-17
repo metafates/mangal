76bfe1be1fdbf875e576aedd092d60de897e8d55
metafates <fates@duck.com>
2022-08-17 20:25:05 +0400

    build: bump version

--- constant/constant.go
+++ constant/constant.go
@@ -2,7 +2,7 @@ package constant
 
 const (
 	Mangal    = "mangal"
-	Version   = "3.2.2"
+	Version   = "3.3.0"
 	UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
 )
 
