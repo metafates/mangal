1e718560d151a8e3b40a0a71c2b34fb1fad98b06
metafates <fates@duck.com>
2022-08-20 10:37:16 +0400

    refactor(env): make env command more compact, add filter flag

--- cmd/env.go
+++ cmd/env.go
@@ -4,6 +4,7 @@ import (
 	"github.com/metafates/mangal/config"
 	"github.com/metafates/mangal/constant"
 	"github.com/metafates/mangal/style"
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 	"os"
 	"strings"
@@ -11,6 +12,7 @@ import (
 
 func init() {
 	rootCmd.AddCommand(envCmd)
+	envCmd.Flags().BoolP("filter", "f", false, "filter out variables that are not set")
 }
 
 var envCmd = &cobra.Command{
@@ -18,20 +20,26 @@ var envCmd = &cobra.Command{
 	Short: "Show available environment variables",
 	Long:  `Show available environment variables.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		filter := lo.Must(cmd.Flags().GetBool("filter"))
+
 		config.EnvExposed = append(config.EnvExposed, "MANGAL_CONFIG_PATH")
 		for _, env := range config.EnvExposed {
+
 			env = strings.ToUpper(constant.Mangal + "_" + config.EnvKeyReplacer.Replace(env))
 			value, present := os.LookupEnv(env)
 
-			cmd.Println(style.Combined(style.Bold, style.Magenta)(env))
+			if !present && filter {
+				continue
+			}
+
+			cmd.Print(style.Combined(style.Bold, style.Magenta)(env))
+			cmd.Print("=")
 
 			if present && value != "" {
 				cmd.Println(style.Green(value))
 			} else {
 				cmd.Println(style.Red("unset"))
 			}
-
-			cmd.Println()
 		}
 	},
 }
