c71e52fa0ae6c4270ea65b16c7b8e9191d926be9
metafates <fates@duck.com>
2022-09-13 19:59:47 +0300

    test: add more tests

---
+++ provider/provider_test.go
@@ -0,0 +1,23 @@
+package provider
+
+import (
+	"github.com/metafates/mangal/provider/manganelo"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	Convey("When trying to get a valid provider", t, func() {
+		_, ok := Get(manganelo.Name)
+		Convey("Then ok should be true", func() {
+			So(ok, ShouldBeTrue)
+		})
+	})
+
+	Convey("When trying to get an invalid provider", t, func() {
+		_, ok := Get("kek")
+		Convey("Then ok should be false", func() {
+			So(ok, ShouldBeFalse)
+		})
+	})
+}
