02c7dd06e36af12f3b3e4f5167817cab423a8a05
metafates <fates@duck.com>
2022-10-31 16:00:09 +0300

    fix: omitempty comicinfo

--- source/comicinfo.go
+++ source/comicinfo.go
@@ -8,15 +8,15 @@ type ComicInfo struct {
 	XmlnsXsd string   `xml:"xmlns:xsd,attr"`
 
 	// General
-	Title      string `xml:"Title"`
-	Series     string `xml:"Series"`
-	Number     int    `xml:"Number"`
-	Web        string `xml:"Web"`
-	Genre      string `xml:"Genre"`
-	PageCount  int    `xml:"PageCount"`
-	Summary    string `xml:"Summary"`
-	Count      int    `xml:"Count"`
-	Characters string `xml:"Characters"`
+	Title      string `xml:"Title,omitempty"`
+	Series     string `xml:"Series,omitempty"`
+	Number     int    `xml:"Number,omitempty"`
+	Web        string `xml:"Web,omitempty"`
+	Genre      string `xml:"Genre,omitempty"`
+	PageCount  int    `xml:"PageCount,omitempty"`
+	Summary    string `xml:"Summary,omitempty"`
+	Count      int    `xml:"Count,omitempty"`
+	Characters string `xml:"Characters,omitempty"`
 	Year       int    `xml:"Year,omitempty"`
 	Month      int    `xml:"Month,omitempty"`
 	Day        int    `xml:"Day,omitempty"`
@@ -24,7 +24,7 @@ type ComicInfo struct {
 	Penciller  string `xml:"Penciller,omitempty"`
 	Letterer   string `xml:"Letterer,omitempty"`
 	Translator string `xml:"Translator,omitempty"`
-	Tags       string `xml:"Tags"`
-	Notes      string `xml:"Notes"`
-	Manga      string `xml:"Manga"`
+	Tags       string `xml:"Tags,omitempty"`
+	Notes      string `xml:"Notes,omitempty"`
+	Manga      string `xml:"Manga,omitempty"`
 }
